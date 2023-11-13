41feb1e2e05fa3a08fdd0e68289a1e4466baa3f7
beginner2023 <1426536274@qq.com>
2023-11-13 17:16:17 +0800

    build java project

---
+++ javawork/src/main/java/com/example/javawork/JavaworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaworkApplication.class, args);
+	}
+
+}
